92ce890e8a9f71bd67eabf6a952dbe45dee89f23
Adarsh <87609950+adarshmarvel22@users.noreply.github.com>
2023-07-08 11:21:24 +0530

    Time: 175 ms (97.25%), Space: 69.1 MB (29.21%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
@@ -10,8 +10,6 @@ public:
         if(n==1||k==1)  return 0;
         
         for(int i=0;i<n-1;i++){
-            if(i==n)  tmp.push_back(1ll*w[i]);
-            else
             tmp.push_back(1ll*w[i]+1ll*w[i+1]);
         }
         
