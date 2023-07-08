582f1d34fe51877c6b524e7495f0b83c3f7326ad
Adarsh <87609950+adarshmarvel22@users.noreply.github.com>
2023-07-08 11:09:45 +0530

    Time: 174 ms (97.25%), Space: 69.1 MB (29.21%) - LeetHub

---
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long putMarbles(vector<int>& w, int k) {
+        vector<long long>tmp;
+        int n=w.size();
+        
+        long long mx=w[0]+w[n-1];
+        long long mi=w[0]+w[n-1];
+        
+        if(n==1||k==1)  return 0;
+        
+        for(int i=0;i<n-1;i++){
+            if(i==n-1)  tmp.push_back(1ll*w[i]);
+            else
+            tmp.push_back(1ll*w[i]+1ll*w[i+1]);
+        }
+        
+        sort(tmp.begin(),tmp.end());
+        
+        for(int i=0;i<k-1;i++){
+            mi+=tmp[i];
+            mx+=tmp[n-2-i];
+        }
+        
+        return mx-mi;
+    }
+};
