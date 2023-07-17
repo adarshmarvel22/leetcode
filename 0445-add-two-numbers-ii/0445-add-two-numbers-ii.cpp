5e634008a1f061ac01ea6a067b55e09275db33b7
Adarsh <87609950+adarshmarvel22@users.noreply.github.com>
2023-07-17 12:51:00 +0530

    Time: 31 ms (89.14%), Space: 71.4 MB (35.96%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -8,81 +8,46 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ //addTwoNumbers
 class Solution {
 public:
-    string add_two_str_no(string n1, string n2){
-        
-        int n=n1.size()-1;
-        int m=n2.size()-1;
-        
-        string ans="";
-        
-          int carry = 0;
-           int sum = 0;
-        
-           while(n >= 0 and m>=0) {
-                int a = n1[n] - '0';
-                int b = n2[m] - '0';
-                sum = (a + b + carry) % 10;
-                carry = (carry + a + b) / 10;
-               
-               ans+=(sum+'0');
-               n--,m--;
-             }
-        
-        while(n>=0){
-                int a = n1[n] - '0';
-                sum = (a + carry) % 10;
-                carry = (carry + a) / 10;
-               ans+=(sum+'0');
-               n--;
-        }
-        
-            while(m>=0) {
-                int b = n2[m] - '0';
-                sum = (b + carry) % 10;
-                carry = (carry + b) / 10;
-               ans+=(sum+'0');
-               m--;
-             }
-        
-        if(carry>0){
-            ans+=(carry+'0');
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
-    }
-    
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-
-          string no1="",no2="";
-        
-        while(l1!=NULL){
-           no1+=(l1->val+'0');
-            l1=l1->next;
-        }
-        
-        while(l2!=NULL){
-           no2+=(l2->val+'0');
-            l2=l2->next;
-        }
-
-        string s=add_two_str_no(no1,no2);
-        cout<<s;
-        
-        ListNode*h=new ListNode();
-    
-        int n=s.size();
-        ListNode*tmp=h;
-        
-        for(int i=0;i<n;i++){
-            tmp->val=s[i]-'0';
-            if(i!=n-1){
-                tmp->next=new ListNode();
-                tmp=tmp->next;
+        ListNode *ptr1 = l1, *ptr2 = l2;
+        while(ptr1 != NULL || ptr2 != NULL){
+            if(ptr1 == NULL){
+                ListNode *newNode = new ListNode(0);//Adding 0's to end of the first list if its size is less than second list
+                newNode->next = l1;
+                l1 = newNode;
+                
+                ptr2 = ptr2->next;
+            }
+            else if(ptr2 == NULL){
+                ListNode *newNode = new ListNode(0); //Adding 0's to end of the second list if its size is less than first list
+                newNode->next = l2;
+                l2 = newNode;
+                ptr1 = ptr1->next;
+            }
+            else{
+                ptr1 = ptr1->next;
+                ptr2 = ptr2->next;
             }
         }
-        
-        return h;
+        int carry = 0;
+        ListNode *dummy = new ListNode(-1);
+        dummy->next = addTwoDigit(l1, l2, carry);
+        if(carry != 0){
+            ListNode *newNode = new ListNode(carry);
+            newNode->next = dummy->next;
+            dummy->next = newNode;
+        }
+        return dummy->next;
+    }
+    ListNode* addTwoDigit(ListNode* l1, ListNode* l2, int &carry){ //recursion is used to perform addition 
+        if(l1 == NULL && l2 == NULL)  return NULL;
+        ListNode *newNode = new ListNode(-1);
+        newNode->next = addTwoDigit(l1->next, l2->next, carry);
+        newNode->val = (l1->val + l2->val + carry) % 10;
+        carry = (l1->val + l2->val + carry) / 10;
+        return newNode;
     }
 };
